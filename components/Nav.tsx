daca6b3f5b0a05cfe6b220f7c92ac87edca473c4
bonniemo <moller.bon@gmail.com>
2025-03-19 13:11:19 +0100

    added icons to nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,17 +7,17 @@ import Settings from "./Settings";
 const Nav = () => {
     return (
         <section className="flex justify-between md:justify-end items-center max-w-md md:max-w-full m-auto md:m-0 gap-8 p-4 mb-8">
-            <Link href="/book-corner" className="flex flex-col items-center">
-                <GiBookshelf />
-                Book corner
+            <Link href="/" className="flex flex-col items-center">
+                <ImHome className="min-w-7 min-h-7" />
+                Home
             </Link>
             <Link href="/search" className="flex flex-col items-center">
-                <IoSearch />
+                <IoSearch className="min-w-7 min-h-7" />
                 Book Search
             </Link>
-            <Link href="/" className="flex flex-col items-center">
-                <ImHome />
-                Home
+            <Link href="/book-corner" className="flex flex-col items-center">
+                <GiBookshelf className="min-w-7 min-h-7" />
+                Book corner
             </Link>
             <div className="md:block hidden">
                 <Settings />
