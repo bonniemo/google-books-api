5b6d0895d00bc11faac915347989dd63ad5472e2
bonniemo <moller.bon@gmail.com>
2025-04-08 21:38:42 +0200

    removed redundant styles

--- components/BookshelfDisplay.tsx
+++ components/BookshelfDisplay.tsx
@@ -65,7 +65,7 @@ const BookshelfDisplay = () => {
     }
 
     return (
-        <div className="container mx-auto">
+        <div>
             {/* Dynamically render checkboxes */}
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-4">
                 {BOOK_CATEGORIES.map(({ key, label }) => (
