a6a533a40f71cddb2825ca5c1ac4cf2642c8424f
bonniemo <moller.bon@gmail.com>
2025-04-10 10:13:03 +0200

    base setup sidebar

---
+++ components/ui/app-sidebar.tsx
@@ -0,0 +1,61 @@
+import { GiBookshelf } from "react-icons/gi";
+import { ImHome } from "react-icons/im";
+import { IoSearch } from "react-icons/io5";
+
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+    {
+        title: "Home",
+        url: "/",
+        icon: ImHome,
+    },
+    {
+        title: "Book Search",
+        url: "/search",
+        icon: IoSearch,
+    },
+    {
+        title: "Book corner",
+        url: "/book-corner",
+        icon: GiBookshelf,
+    },
+];
+
+export function AppSidebar() {
+    return (
+        <Sidebar>
+            <SidebarTrigger />
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>Book Scribble</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {items.map((item) => (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild>
+                                        <a href={item.url}>
+                                            <item.icon />
+                                            <span>{item.title}</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+        </Sidebar>
+    );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,44 +8,54 @@ export default {
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
-        extend: {
-            colors: {
-                base: {
-                    night: "#0E0E11",
-                    light: "#F2F9F9",
-                    dark: "#1A2C32",
-                },
-                accent: {
-                    night: "#18181B",
-                    dark: "#3C747E",
-                    light: "#C0DFE1",
-                    accent: "#79B6BC",
-                    soft: "#94C8CC",
-                },
-            },
-            keyframes: {
-                "accordion-down": {
-                    from: {
-                        height: "0",
-                    },
-                    to: {
-                        height: "var(--radix-accordion-content-height)",
-                    },
-                },
-                "accordion-up": {
-                    from: {
-                        height: "var(--radix-accordion-content-height)",
-                    },
-                    to: {
-                        height: "0",
-                    },
-                },
-            },
-            animation: {
-                "accordion-down": "accordion-down 0.2s ease-out",
-                "accordion-up": "accordion-up 0.2s ease-out",
-            },
-        },
+    	extend: {
+    		colors: {
+    			base: {
+    				night: '#0E0E11',
+    				light: '#F2F9F9',
+    				dark: '#1A2C32'
+    			},
+    			accent: {
+    				night: '#18181B',
+    				dark: '#3C747E',
+    				light: '#C0DFE1',
+    				accent: '#79B6BC',
+    				soft: '#94C8CC'
+    			},
+    			sidebar: {
+    				DEFAULT: 'hsl(var(--sidebar-background))',
+    				foreground: 'hsl(var(--sidebar-foreground))',
+    				primary: 'hsl(var(--sidebar-primary))',
+    				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+    				accent: 'hsl(var(--sidebar-accent))',
+    				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+    				border: 'hsl(var(--sidebar-border))',
+    				ring: 'hsl(var(--sidebar-ring))'
+    			}
+    		},
+    		keyframes: {
+    			'accordion-down': {
+    				from: {
+    					height: '0'
+    				},
+    				to: {
+    					height: 'var(--radix-accordion-content-height)'
+    				}
+    			},
+    			'accordion-up': {
+    				from: {
+    					height: 'var(--radix-accordion-content-height)'
+    				},
+    				to: {
+    					height: '0'
+    				}
+    			}
+    		},
+    		animation: {
+    			'accordion-down': 'accordion-down 0.2s ease-out',
+    			'accordion-up': 'accordion-up 0.2s ease-out'
+    		}
+    	}
     },
     plugins: [],
 } satisfies Config;
